5b9a1ba489067caabbe8b65d43a81a20d9a4ee07
shawanaGideon <gshawana@my.centennialcollege.ca>
2024-07-08 10:22:12 -0400

    Tested Api

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,6 +1,6 @@
 const User = require("../model/User.js");
 
-const createEmail = async (res, req) => {
+const createEmail = async (req, res) => {
   try {
     const user = await User.create(req.body);
     res.status(201).json(user);
@@ -9,10 +9,10 @@ const createEmail = async (res, req) => {
   }
 };
 
-const getEmails = async (res, req) => {
+const getEmails = async (req, res) => {
   try {
-    const user = await User.find({});
-    res.status(200).json(user);
+    const users = await User.find({});
+    res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
