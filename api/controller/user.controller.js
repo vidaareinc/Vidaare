0431285bb599b89d721308e6b7b5b8eb35c59057
shawanaGideon <gshawana@my.centennialcollege.ca>
2024-07-08 10:22:12 -0400

    Tested Api

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -29,8 +29,8 @@ const createEmail = async (req, res) => {
 
 const getEmails = async (req, res) => {
   try {
-    const user = await User.find({});
-    res.status(200).json(user);
+    const users = await User.find({});
+    res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
