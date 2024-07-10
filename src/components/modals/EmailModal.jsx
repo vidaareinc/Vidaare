60b9cb634a0db4e07529c6530c3aecf69df92114
shawanaGideon <gshawana@my.centennialcollege.ca>
2024-07-10 13:39:13 -0400

    change x-mark color to white

--- src/components/modals/EmailModal.jsx
+++ src/components/modals/EmailModal.jsx
@@ -73,6 +73,7 @@ export default function EmailModal({ isOpen, onClose }) {
               strokeLinecap="round"
               strokeLinejoin="round"
               d="M6 18L18 6M6 6l12 12"
+              color="white"
             />
           </svg>
         </button>
