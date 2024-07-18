5a2f93b03cbf4c9752ed0a45d7b9e184dfb3ccd6
Mark Jay <marksumoba@gmail.com>
2024-07-18 09:54:56 -0400

    MyVideo Section Added

---
+++ client/src/components/videos/MyRecentlyPlayedCard.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MyRecentlyPlayedCard() {
+  return <div></div>;
+}
--- client/src/pages/MyVideos.jsx
+++ client/src/pages/MyVideos.jsx
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import MyRecentlyPlayed from "../components/videos/MyRecentlyPlayed";
+import MyVideoList from "../components/videos/MyVideoList";
 
 export default function MyVideos() {
+  const [filter, setFilter] = useState("all");
+
+  const filters = [
+    "all",
+    "educational videos",
+    "entertaining videos",
+    "promotional videos",
+    "live streams",
+    "event/coverage videos",
+    "how-to/guide videos",
+    "personal/vlog videos",
+    "others",
+  ];
+
   return (
-    <div>
-      <h1>My Videos Here</h1>
-    </div>
+    <>
+      <div className="flex flex-row justify-evenly w-full">
+        {filters.map((filterItem) => (
+          <h1
+            key={filterItem}
+            className={`text-sm cursor-pointer uppercase mr-4 text-white ${
+              filter === filterItem ? "font-bold" : ""
+            }`}
+            onClick={() => setFilter(filterItem)}
+          >
+            {filterItem}
+          </h1>
+        ))}
+      </div>
+      <div className="m-4">
+        <MyRecentlyPlayed filter={filter} />
+      </div>
+      <div className="flex flex-wrap px-8">
+        <MyVideoList filter={filter} />
+      </div>
+    </>
   );
 }
